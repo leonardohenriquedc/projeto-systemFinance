f290f8a7397ff0c39c551887cc6883887268bb67
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-03-18 11:46:45 -0400

    unindo documentos JS

--- arquivosJS/intJsDesktop.js
+++ arquivosJS/intJsDesktop.js
@@ -8,6 +8,13 @@ let menuBurger = document.getElementsByClassName('menuBurguer');
 let navMenuLateral = document.getElementById('paiMenuOptions');
 let navButtonFrenteCaixa = document.querySelector('#navFrenteCaixa');
 let navButtonFluxoCaixa = document.querySelector('#navFluxoCaixa');
+let selectValue = document.querySelector('relatoriosSelect');
+
+let sectionRelatorios = document.getElementById('relatorios');
+let subSectionPlanoContas = document.getElementById('planoContas');
+let subSectionContasPagarReceber = document.getElementById('contarPagarEReceber');
+let subArticleContasPagar = document.getElementById('contasPagar');
+let subArticleContasReceber = document.getElementById('contasReceber');
 
 
 // Configuracao lista header
@@ -26,7 +33,7 @@ function menuLateral() {
             navMenuLateral.classList.add('fora');
             navMenuLateral.classList.remove('dentro');
         }
-        console.log('ta funcionando')
+        console.log('ta funcionando 2')
 }
 
 for (const menusBurguer of menuBurger) {
@@ -44,4 +51,60 @@ function openFrenteCaixa() {
 }
 
 navButtonFrenteCaixa.addEventListener('click', openFrenteCaixa)
- 
+
+
+
+
+let mobileMenuBurger = document.getElementsByClassName("menuBurguer");
+let listUl = document.getElementsByClassName("list");
+
+let listLiOne = document.getElementById('SobreNosTelaInical');
+let listLiTwo = document.querySelector("#LiTwo");
+let sectionTelaApresentacao = document.getElementById('telaApresentacao');
+let sectionTelaSobreNos = document.getElementById('telaSobreNos');
+let navMenuLateral2 = document.getElementById('paiMenuOptions');
+
+if (window.innerWidth <= 600) {
+    navMenuLateral2.style.display = 'none'
+}
+
+// Abertura Menu
+function menu() {
+    if (window.innerWidth <= 600) {
+        for (lists of listUl) {
+            if (lists.style.display === 'none') {
+                lists.style.display = 'block'
+            }
+            else {
+                lists.style.display = 'none'
+            }
+        }
+        console.log('ta funcionando')
+    }
+}
+
+for (const botoesBurguer of mobileMenuBurger) {
+    botoesBurguer.addEventListener('click', menu)
+}
+
+// transicao abas
+
+function transicaoAbas() {
+    if (sectionTelaApresentacao.style.display === 'flex' ||
+        sectionTelaSobreNos.style.display === 'none'
+    ) {
+        sectionTelaApresentacao.style.display = 'none';
+        sectionTelaSobreNos.style.display = 'flex';
+        listLiTwo.innerHTML = 'Voltar'
+    }
+    else {
+        sectionTelaApresentacao.style.display = 'flex';
+        sectionTelaSobreNos.style.display = 'none';
+        listLiTwo.innerHTML = 'SobreNos'
+    }
+    console.log('funcionando')
+}
+
+
+listLiOne.addEventListener('click', transicaoAbas)
+listLiTwo.addEventListener('click', transicaoAbas)
